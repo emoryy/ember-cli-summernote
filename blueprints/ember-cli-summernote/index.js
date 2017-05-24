d23259728687c5e4842aad1bb610a5aa4ba3da5c
Barta Gábor <emoryy@gmail.com>
2017-05-24 16:42:39 +0200

    change summernote version to 0.8.3 in blueprint

--- blueprints/ember-cli-summernote/index.js
+++ blueprints/ember-cli-summernote/index.js
@@ -2,18 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    var that = this;
-
-
-    return that.addBowerPackageToProject('summernote', "~0.7.0");
-    //
-    // return this.addBowerPackageToProject('rangy-1.3').then(function() {
-    //   return this.addBowerPackageToProject('wysihtml5x').then(function() {
-    //     return this.addBowerPackageToProject('bootstrap').then(function() {
-    //       return that.addBowerPackageToProject('bootstrap3-wysihtml5-bower');
-    //     });
-    //   });
-    // });
-
+    return this.addBowerPackageToProject('summernote', "~0.8.3");
   }
 };
