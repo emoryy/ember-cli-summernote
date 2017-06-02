14a062590598608682cf0c76b8ec59f5b918da0f
Barta Gábor <emoryy@gmail.com>
2017-06-02 12:23:41 +0200

    bootstrap as npm dependency

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ module.exports = {
           include: ['summernote.eot', 'summernote.ttf', 'summernote.woff'],
           destDir: 'assets/font'
         }
+      },
+      bootstrap: {
+        vendor: {
+          srcDir: 'dist',
+          include: ['css/bootstrap.min.css', 'js/bootstrap.min.js']
+        }
       }
     }
   },
@@ -33,11 +39,14 @@ module.exports = {
 
     // Import Bootstrap
     if (options.importBootstrapCSS) {
-      app.import(path.join(bootstrapPath, '/css/bootstrap.min.css'));
+      // app.import(path.join(bootstrapPath, '/css/bootstrap.min.css'));
+      app.import('vendor/bootstrap/bootstrap.min.css');
+
     }
 
     if (options.importBootstrapJS) {
-      app.import(path.join(bootstrapPath, '/js/bootstrap.min.js'));
+      // app.import(path.join(bootstrapPath, '/js/bootstrap.min.js'));
+      app.import('vendor/bootstrap/bootstrap.min.js');
     }
 
     // Import css and glyphicons from FontAwesome
